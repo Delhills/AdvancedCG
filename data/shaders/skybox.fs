6b7231ce198ae0d94ecedc9d3364a01711eb6fb4
ArnauGonzalez-upf <arnau.gonzalez02@estudiant.upf.edu>
2021-10-05 01:16:57 +0200

    mas cambios

--- data/shaders/skybox.fs
+++ data/shaders/skybox.fs
@@ -1,12 +1,11 @@
 varying vec3 v_world_position;
 
-uniform vec4 u_color;
 uniform samplerCube u_texture;
 uniform vec3 u_camera_position;
 
 void main()
 {
-	vec3 V = v_world_position - u_camera_position;
+	vec3 V = normalize(v_world_position - u_camera_position);
 	vec4 color = textureCube(u_texture, V);
 	gl_FragColor = color;
 }
